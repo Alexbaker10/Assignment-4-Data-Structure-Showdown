d4f18193cadd2f8ef2b3cf37ff2dd6cd93fffbab
Alexbaker10 <161655042+Alexbaker10@users.noreply.github.com>
2025-09-06 10:05:41 -0400

    Update timed_challenge.py

--- timed_challenge.py
+++ timed_challenge.py
@@ -1,3 +1,18 @@
 # Pick one question from timed_challenge.txt
 # Paste the question as a comment below
 # Set a timer for 30 minutes and complete the question!
+"""3. Remove Duplicates (Keep Order)
+Return the values in the order they first appeared, without duplicates.
+Input: ["apple", "banana", "apple", "kiwi", "banana"]
+Output: ["apple", "banana", "kiwi"]"""
+def remove_duplicate(input_list):
+  results = []
+  seen = set()
+  for item in input_list:
+    if item not in seen:
+      results.append(item)
+      seen.add(item)
+  return results
+data = ["apple", "banana", "apple", "kiwi", "banana"]
+output = remove_duplicate(data)
+print(output)
